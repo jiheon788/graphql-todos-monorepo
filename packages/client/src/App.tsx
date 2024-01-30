2d8d53ca00b643d12c7f55eacddb65a9950f9d67
jiheon_park <jiheon_park@tmax.co.kr>
2024-01-30 17:42:56 +0900

    feat: create, read todo

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { useQuery, useMutation, gql } from "@apollo/client";
+
+// GraphQL 쿼리 정의
+const GET_TODOS = gql`
+  query GetTodos {
+    todos {
+      id
+      text
+      completed
+    }
+  }
+`;
+
+// GraphQL 뮤테이션 정의
+const ADD_TODO = gql`
+  mutation AddTodo($text: String!) {
+    addTodo(text: $text) {
+      id
+      text
+      completed
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data, refetch } = useQuery(GET_TODOS);
+  const [value, setValue] = useState("");
+  const [addTodo] = useMutation(ADD_TODO);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <h3>My Todo List</h3>
+        <input
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            addTodo({ variables: { text: value } }).then((res) => {
+              refetch();
+            });
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add Todo
+        </button>
+        <ul>
+          {data.todos.map((todo: { id: string; text: string }) => (
+            <li key={todo.id}>{todo.text}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
