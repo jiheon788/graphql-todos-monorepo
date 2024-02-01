e69a908d92f7ec04be35ba631b03e14d981431b9
jiheon_park <jiheon_park@tmax.co.kr>
2024-02-01 18:27:12 +0900

    docs: update comment using jsDoc

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import "./App.css";
 import { useQuery, useMutation, gql } from "@apollo/client";
 
-// GraphQL 쿼리 정의
+/**
+ * GraphQL 쿼리 정의
+ */
 const GET_TODOS = gql`
   query GetTodos {
     todos {
@@ -13,7 +15,10 @@ const GET_TODOS = gql`
   }
 `;
 
-// GraphQL 뮤테이션 정의
+/**
+ * GraphQL AddTodo 뮤테이션 정의
+ *
+ */
 const ADD_TODO = gql`
   mutation AddTodo($text: String!) {
     addTodo(text: $text) {
@@ -24,6 +29,11 @@ const ADD_TODO = gql`
   }
 `;
 
+/**
+ * GraphQL TOGGLE_TODO 뮤테이션 정의
+ * @params $id: ID! 이 뮤테이션을 호출할 때 id라는 이름의 변수를 제공해야 하며, 이 변수는 null이 될 수 없는 ID 타입의 값이어야 한다
+ * @returns { id, text, completed } 서버로부터 반환받을 데이터를 지정
+ */
 const TOGGLE_TODO = gql`
   mutation ToggleTodo($id: ID!) {
     toggleTodo(id: $id) {
@@ -51,8 +61,8 @@ function App() {
   const [addTodo] = useMutation(ADD_TODO, {
     /**
      * 낙관적 업데이트는 서버 응답이 성공할 것이라고 "낙관적"으로 가정하고, 그에 따라 UI를 미리 업데이트
-     * 1. 뮤테이션이 서버로 전송되고 서버로부터 응답을 받기 전에 사용할 임시 응답을 정의
-     * 2. 임시 응답은 실제 서버 응답이 도착할 때까지 UI에 표시
+     * 뮤테이션이 서버로 전송되고 서버로부터 응답을 받기 전에 사용할 임시 응답을 정의
+     * 임시 응답은 실제 서버 응답이 도착할 때까지 UI에 표시
      */
     optimisticResponse: {
       addTodo: {
@@ -62,16 +72,17 @@ function App() {
         __typename: "Todo",
       },
     },
+
     /**
      * 뮤테이션이 성공적으로 완료된 후 호출
      * Apollo 캐시를 직접 조작하여, 뮤테이션의 결과를 반영
      * 서버로부터 받은 최종 데이터로 클라이언트의 UI를 업데이트
+     * 1. 캐시에서 현재의 todos 쿼리 결과를 가져온다
+     * 2. 새로운 todo를 todos 배열에 추가
      */
     update: (cache, { data: { addTodo } }) => {
-      // 캐시에서 현재의 todos 쿼리 결과를 가져옵니다.
       const currentData = cache.readQuery<TodoType>({ query: GET_TODOS });
 
-      // 새로운 todo를 todos 배열에 추가
       if (currentData) {
         cache.writeQuery({
           query: GET_TODOS,
